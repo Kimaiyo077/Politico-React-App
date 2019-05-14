ebf00c5fd39fd3bbabb66c13a13a40003726c841
Isaac Kimaiyo <kimaiyo@Isaacs-MacBook-Pro.local>
2019-05-14 23:21:08 +0300

    Add starter code

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render(){
+        return(<div>
+            <h1>Politico React Application</h1>
+        </div>);
+    }
+}
+
+export default App;
